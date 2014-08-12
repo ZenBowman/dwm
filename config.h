161c4000b816fba32a22ff622aeb2a797851a0c5
Prasan Samtani <prasansamtani@gmail.com>
2014-08-12 16:20:21 -0700

    Removed modkey for floating layout - down with floating!

--- config.h
+++ config.h
@@ -71,7 +71,9 @@ static Key keys[] = {
 
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+
+       // No need for floating, this interferes with my IntelliJ menu
+	// { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
